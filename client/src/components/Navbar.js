1f5247702847bb32d2fc8e50b474ef83032cd1d4
Jose <pjose14.jp38@yahoo.com>
2020-03-22 21:32:09 -0500

    working app. simple styling with styled-components

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,1 +1,25 @@
 import React from 'react';
+import { useDarkMode } from './hooks/useDarkMode';
+
+const Navbar = () => {
+    const [darkMode, setDarkMode] = useDarkMode(false);
+
+    const toggleMode = e => {
+        e.preventDefault();
+        setDarkMode(!darkMode);
+    };
+
+    return (
+        <nav>
+            <h1>Women's World Cup Players</h1>
+            <div className="dark-mode_toggle">
+                <div
+                    onClick={toggleMode}
+                    className={darkMode ? 'toggle toggled' : 'toggle'}
+                />
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
--- client/src/components/Player.js
+++ client/src/components/Player.js
@@ -1,11 +1,36 @@
-import React, { Component } from 'react'
+import React from 'react'
+import styled from 'styled-components'
 
-export default class Player extends Component {
-    render(props) {
-        return (
-            <div>
-                Name: { this.props.player.name }
-            </div>
-        )
+const Player = ({ player }) => {
+    return (
+      <PlayerWrapper data-testid="player-test-id">
+        <h1>{player.name}</h1>
+        <h3>{player.country}</h3>
+        <p>{player.searches}</p>
+      </PlayerWrapper>
+    )
+  }
+  
+  export default Player
+  
+  const PlayerWrapper = styled.div`
+    width: 30%;
+    margin: 1rem auto;
+    text-align: center;
+    padding: 1rem 2rem 1rem;
+    border: .5px solid gray;
+    border-radius: 8px;
+    opacity: .8;
+    h1 {
+      font-size: 2.5rem;
+      padding: 1rem 0;
     }
-}
+    h3 {
+      font-size: 1.8rem;
+      padding: 1rem 0;
+    }
+    p {
+      font-size: 1.2rem;
+      padding: 1rem 0;
+    }
+  `
